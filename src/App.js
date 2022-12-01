2f48e2d93ce56cfb3904416e0e0c4cae5777af8a
liubingao <410497230@qq.com>
2022-12-01 14:08:13 +0800

    路由,状态机初始化

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, NavLink, Outlet } from 'react-router-dom'
+import { Provider } from 'mobx-react'
+import  RootStore  from './stores/index'
+
+import Home from './pages/Home/Home.jsx'
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider {...new RootStore()}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />}>
+              <Route path='' element={<div>消息</div>}></Route>
+              <Route path='face' element={<div>视讯会议</div>}></Route>
+              <Route path='maliList' element={<div>通讯录</div>}></Route>
+              <Route path='work' element={<div>工作台</div>}></Route>
+              <Route path='doc' element={<div>云文档</div>}></Route>
+              <Route path='more' element={<div>更多</div>}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
